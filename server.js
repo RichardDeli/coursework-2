38fcaa295f724837acaedc63199e71de5793c3f6
RichardD <richarddeliha@gmail.com>
2023-02-16 17:23:56 +0000

    setup database connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDb } = require("./db");
+const logger = require("./logger");
+
+let connection;
+
+const uri = 'mongodb+srv://Richard:richard1-@coursework-2.kklxmf0.mongodb.net/?retryWrites=true&w=majority'
+
+const connectToDb = async () => {
+    connection = await MongoClient.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, }
+      );
+};
+
+const getDb = () => {
+    
+    return connection.db('coursework');
+  };
+
+  
